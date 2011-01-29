f53ac3b5492138e46a60a315353dbdcf831d4a3a
Mens <Mens@Mens-PC>
2011-01-30 00:21:00 +0100

    Editor mit Soundausgabe versehen

--- src/clemensNeu/Sound.java
+++ src/clemensNeu/Sound.java
@@ -6,13 +6,46 @@ import java.net.MalformedURLException;
 
 public class Sound extends Applet {
       
-	@SuppressWarnings("deprecation")
-	public void playSound(char letter){
-		System.out.println(letter);
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3867379223432272441L;
+
+	public static void playSound(char letter){
+		
 		File file= new File("src/Audio/" + letter+ ".wav");
 		AudioClip sound = null;
 		try {
-			sound = Applet.newAudioClip(file.toURL());
+			sound = Applet.newAudioClip(file.toURI().toURL());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		sound.play();
+	}
+	
+	public static void playSound(int letter){
+		
+		File file= new File("src/Audio/" + letter+ ".wav");
+		AudioClip sound = null;
+		try {
+			sound = Applet.newAudioClip(file.toURI().toURL());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		sound.play();
+	}
+	
+	public static void playSound(String letter){
+		
+		File file= new File("src/Audio/" + letter+ ".wav");
+		AudioClip sound = null;
+		try {
+			sound = Applet.newAudioClip(file.toURI().toURL());
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
